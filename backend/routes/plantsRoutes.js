52193189a73acbae0fb72d2a24941a53284ba605
Tarif205 <tarifrahman93@gmail.com>
2025-12-14 20:58:05 +0600

    second module done

--- backend/routes/plantsRoutes.js
+++ backend/routes/plantsRoutes.js
@@ -2,11 +2,6 @@
 const express = require('express');
 const router = express.Router();
 const {getPlants, getPlantsId} = require('../controllers/plantsController');
-
-
-
-
-
 router.get('/', getPlants);
 router.get('/:id', getPlantsId);
 
