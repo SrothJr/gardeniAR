a67bafe830bfc73450c427b8ac0cc2c88f91eae1
SrothJr <nazimhossain14602@gmail.com>
2025-12-23 22:20:36 +0600

    Merged but issue, checking out main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ExplorePlants from './pages/ExplorePlants';
 import PlantDetail from './pages/PlantDetail';
+import WeedIdentifier from './components/WeedIdentifier';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<ExplorePlants />} />
         <Route path="/plants/:id" element={<PlantDetail />} />
+        <Route path="/identify" element={<WeedIdentifier />} />
       </Routes>
     </BrowserRouter>
   );
--- mobile/config.js
+++ mobile/config.js
@@ -1,3 +1,3 @@
-export const nBACKEND = "http://192.168.0.192:5000";
-export const BACKEND = "http://192.168.0.192:5000";
+export const nBACKEND = "http://192.168.0.113:5000";
+export const BACKEND = "http://192.168.0.113:5000";
 export const BACKEND_URL = BACKEND;
